e5b2fee1130d93c0406a776ff08299ea4888269c
Gregory Paciga <gpaciga@gmail.com>
2021-02-16 17:45:58 -0500

    initial build with Forge, does not do anything yet

---
+++ src/main/java/dev/gerg/minecraft/moonphases/Phases.java
@@ -0,0 +1,35 @@
+package dev.gerg.minecraft.moonphases;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("fixedmoonphases")
+public class Phases {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Phases() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Init Fixed Moon Phases mod");
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
+
+}
